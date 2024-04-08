fd8f876adc8cd19f8c15dcba8d75f00d7936dfd1
Swathi Katakam <swathik2322@gmail.com>
2024-04-08 10:41:22 -0500

    Phase 3 changes

--- Program.cs
+++ Program.cs
@@ -12,19 +12,32 @@ Wanna play a game?
 Guess the secret number:"
 );
 
-int userInput = int.Parse(Console.ReadLine()!.Trim());
 
-Console.WriteLine($"You guessed: {userInput}");
 
 // The program should be updated to...
 
 int secretNumber = 42;
+int countChance=0;
+while(countChance!=4)
+{
+    int userInput = int.Parse(Console.ReadLine()!.Trim());
 
-if (userInput == secretNumber)
-{
-    Console.WriteLine("You guessed it right!!");
+// Console.WriteLine($"You guessed: {userInput}");
+    if (userInput == secretNumber)
+    {
+        Console.WriteLine("You guessed it right!!");
+        countChance=4;
+    }
+    else
+    {
+        Console.WriteLine("You guessed WRONG....guess again:");
+        countChance++;
+        // secretNumber=int.Parse(Console.ReadLine());
+    }
 }
-else
-{
-    Console.WriteLine("You guessed WRONG....guess again:");
-}
+
+// The program should be updated to...
+
+
+// Give the user four chances to guess the number.
+// Continue to display the success or failure messages as in phase 2
