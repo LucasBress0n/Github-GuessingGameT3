c68596be078f4039ed28af91257cd200e02321a6
Lucas Bresson <pandaboy816@gmail.com>
2024-04-08 11:45:02 -0500

    Implemented Phase 7

--- Program.cs
+++ Program.cs
@@ -1,43 +1,78 @@
-﻿
+﻿using System.Collections;
 
-Console.WriteLine(
-    @"
-Wanna play a game?
-Guess the secret number between 1 and 100:"
-);
-int countChance = 0;
-
-Random random = new Random();
-
-int secretNumber = random.Next(1, 101);
-
-while (countChance != 4)
+int userDifficultyChoice = 0;
+while (userDifficultyChoice == 0 || userDifficultyChoice > 3)
 {
-    int userInput = int.Parse(Console.ReadLine()!.Trim());
+    Console.WriteLine(
+        @"
+    Wanna play a game? 
+    pick your difficulty.
 
-
-    if (userInput == secretNumber)
+    1. Easy, guesses: 8
+    2. Medium, guesses: 6
+    3. Hard, guesses: 4
+    
+    Pick a difficulty"
+    );
+    userDifficultyChoice = int.Parse(Console.ReadLine()!.Trim());
+    switch (userDifficultyChoice)
     {
-        Console.WriteLine("You guessed it right!!");
-        countChance = 4;
-    }
-    else
-    {
-        if(userInput>secretNumber)
-        {
-            Console.WriteLine("Your guess is too high, guess lower");
-        }
-        else if(userInput<secretNumber)
-        {
-            Console.WriteLine("Your guess is too low, guess higher");
-        }
-        
-        countChance++;
-        Console.WriteLine(
-            $@"You're on guess number: {countChance}. You have {4 - countChance} chances left!");
-
-       
+        case 1:
+            game(8);
+            break;
+        case 2:
+            game(6);
+            break;
+        case 3:
+            game(4);
+            break;
     }
 }
 
-// Inform the user if their guess was too high or too low, when they guess incorrectly.
+void game(int guessesNumber)
+{
+    Console.WriteLine(
+        @"
+Guess the secret number between 1 and 100:"
+    );
+    int countChance = 0;
+    Random random = new Random();
+
+    int secretNumber = random.Next(1, 101);
+
+    while (countChance != guessesNumber)
+    {
+        int userInput = int.Parse(Console.ReadLine()!.Trim());
+
+        if (userInput == secretNumber)
+        {
+            Console.WriteLine("You guessed it right!!");
+            countChance = guessesNumber;
+        }
+        else
+        {
+            if (userInput > secretNumber)
+            {
+                Console.WriteLine("Your guess is too high, guess lower");
+            }
+            else if (userInput < secretNumber)
+            {
+                Console.WriteLine("Your guess is too low, guess higher");
+            }
+
+            countChance++;
+            Console.WriteLine(
+                $@"You're on guess number: {countChance}. You have {guessesNumber - countChance} chances left!"
+            );
+        }
+    }
+}
+
+
+
+// Prompt the user for a difficulty level before they are prompted to guess the number.
+// The difficulty level should determine how many guesses the user gets. The difficulty levels should be:
+// Easy - this gives the user eight guesses.
+// Medium - this gives the user six guesses.
+// Hard - this gives the user four guesses.
+// Phase 8
